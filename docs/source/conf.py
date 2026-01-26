8b5955501393b34392241ca228b8ee46b4ecfe2f
James Cribb <47155815+CribbJ@users.noreply.github.com>
2026-01-26 11:12:36 +0000

    Sphinx-quickstart init & updates to conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'psql-boilerplate'
+copyright = '2026, James Cribb'
+author = 'James Cribb'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon", # Google/NumPy docstring support
+    "sphinx.ext.viewcode", # adds links to source code
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Add project root to sys path
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../src"))
